e498e4bf12f1b7311cc271616ef1ee03cd3427f9
Sonata8 <wuwenhao2007@gmail.com>
2025-12-07 11:00:49 +0800

    lab12 finished. CS61A ALL FINISHED!!! Wooh!

--- Lab/lab12/lab12.sql
+++ Lab/lab12/lab12.sql
@@ -1,38 +1,54 @@
 CREATE TABLE finals AS
-  SELECT "RSF" AS hall, "61A" as course UNION
-  SELECT "Wheeler"    , "61A"           UNION
-  SELECT "Pimentel"   , "61A"           UNION
-  SELECT "Li Ka Shing", "61A"           UNION
-  SELECT "Stanley"    , "61A"           UNION
-  SELECT "RSF"        , "61B"           UNION
-  SELECT "Wheeler"    , "61B"           UNION
-  SELECT "Morgan"     , "61B"           UNION
-  SELECT "Wheeler"    , "61C"           UNION
-  SELECT "Pimentel"   , "61C"           UNION
-  SELECT "Soda 310"   , "61C"           UNION
-  SELECT "Soda 306"   , "10"            UNION
-  SELECT "RSF"        , "70";
+  SELECT 'RSF' AS hall, '61A' as course UNION
+  SELECT 'Wheeler'    , '61A'           UNION
+  SELECT 'Pimentel'   , '61A'           UNION
+  SELECT 'Li Ka Shing', '61A'           UNION
+  SELECT 'Stanley'    , '61A'           UNION
+  SELECT 'RSF'        , '61B'           UNION
+  SELECT 'Wheeler'    , '61B'           UNION
+  SELECT 'Morgan'     , '61B'           UNION
+  SELECT 'Wheeler'    , '61C'           UNION
+  SELECT 'Pimentel'   , '61C'           UNION
+  SELECT 'Soda 310'   , '61C'           UNION
+  SELECT 'Soda 306'   , '10'            UNION
+  SELECT 'RSF'        , '70';
 
 CREATE TABLE sizes AS
-  SELECT "RSF" AS room, 900 as seats    UNION
-  SELECT "Wheeler"    , 700             UNION
-  SELECT "Pimentel"   , 500             UNION
-  SELECT "Li Ka Shing", 300             UNION
-  SELECT "Stanley"    , 300             UNION
-  SELECT "Morgan"     , 100             UNION
-  SELECT "Soda 306"   , 80              UNION
-  SELECT "Soda 310"   , 40              UNION
-  SELECT "Soda 320"   , 30;
+  SELECT 'RSF' AS room, 900 as seats    UNION
+  SELECT 'Wheeler'    , 700             UNION
+  SELECT 'Pimentel'   , 500             UNION
+  SELECT 'Li Ka Shing', 300             UNION
+  SELECT 'Stanley'    , 300             UNION
+  SELECT 'Morgan'     , 100             UNION
+  SELECT 'Soda 306'   , 80              UNION
+  SELECT 'Soda 310'   , 40              UNION
+  SELECT 'Soda 320'   , 30;
 
 CREATE TABLE sharing AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.course, COUNT(DISTINCT b.hall) AS shared 
+  FROM  finals AS a, finals AS b
+  WHERE a.hall=b.hall AND a.course!=b.course 
+  GROUP BY a.course
+;
 
 CREATE TABLE pairs AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT '' || hall1.room || ' and ' || hall2.room || ' together have ' || (hall1.seats+hall2.seats) || ' seats' AS rooms
+  FROM sizes AS hall1, sizes AS hall2
+  WHERE hall1.seats+hall2.seats >= 1000 AND hall1.room < hall2.room
+  ORDER BY hall1.seats+hall2.seats DESC
+;
 
 CREATE TABLE big AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT course
+  FROM finals, sizes
+  WHERE finals.hall = sizes.room
+  GROUP BY course
+  HAVING SUM(seats) >= 1000
+;
 
 CREATE TABLE remaining AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT course, (SUM(seats) - MAX(seats)) AS remaining
+  FROM finals, sizes  
+  WHERE finals.hall = sizes.room
+  GROUP BY course
+;
